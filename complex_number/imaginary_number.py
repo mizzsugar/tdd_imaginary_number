bad52778a1c2be5c8d17535100c1e8fca7793c85
Mizuki Sato <mizki.sato.0025@gmail.com>
2019-02-15 00:26:03 +0900

    純虚数と虚数の共役と等価性の統合をした
    純虚数は虚数の一部なので、純虚数に虚数を継承させた

--- complex_number/imaginary_number.py
+++ complex_number/imaginary_number.py
@@ -1,21 +1,34 @@
+from __future__ import annotations
 from dataclasses import dataclass
+from typing import Any
 
-from complex_number.purely_imaginary_number import PurelyImaginaryNumber
+from complex_number.myint import MyInt
 
 
 @dataclass
 class ImaginaryNumber:
     real: int
-    imaginary: PurelyImaginaryNumber
+    imaginary: MyInt
 
-    def __init__(self, real: int, imaginary: PurelyImaginaryNumber):
+    def __init__(self, real: int, imaginary: MyInt):
         self.real = real
         self.imaginary = imaginary
 
+    def __eq__(self, other: Any) -> bool:
+        return (
+                isinstance(other, ImaginaryNumber)
+                and self.real == other.real
+                and self.imaginary == other.imaginary
+        )
+
     def __str__(self):
         if self.real == 0:
-            return str(self.imaginary)
-        if self.imaginary.myint.num < 0:
-            num = -1 * self.imaginary.myint.num
+            return f"{self.imaginary.num}i"
+        if self.imaginary.num < 0:
+            num = -1 * self.imaginary.num
             return f"{self.real} - {num}i"
-        return f"{self.real} + {self.imaginary}"
+        return f"{self.real} + {self.imaginary.num}i"
+
+    def is_conjugate_to(self, other: ImaginaryNumber) -> bool:
+        return self.real == other.real \
+               and self.imaginary.num == -1 * other.imaginary.num
--- tests/test_imaginary_number.py
+++ tests/test_imaginary_number.py
@@ -8,39 +8,49 @@ from complex_number.purely_imaginary_number import PurelyImaginaryNumber
 class TestImaginaryNumber(unittest.TestCase):
     def test_create_imaginary_number(self):
         with self.subTest('2 + 4i'):
-            i4 = PurelyImaginaryNumber(MyInt(4))
-            i2_4 = ImaginaryNumber(2, i4)
+            i2_4 = ImaginaryNumber(2, MyInt(4))
             expected = '2 + 4i'
             actual = str(i2_4)
             self.assertEqual(expected, actual)
 
         with self.subTest('2 - 4i'):
-            minus_i4 = PurelyImaginaryNumber(MyInt(-4))
-            i2_minus4 = ImaginaryNumber(2, minus_i4)
+            i2_minus4 = ImaginaryNumber(2, MyInt(-4))
             expected = '2 - 4i'
             actual = str(i2_minus4)
             self.assertEqual(expected, actual)
 
         with self.subTest('0 + 4i'):
-            i4 = PurelyImaginaryNumber(MyInt(4))
-            i0_4 = ImaginaryNumber(0, i4)
+            i0_4 = ImaginaryNumber(0, MyInt(4))
             expected = '4i'
             actual = str(i0_4)
             self.assertEqual(expected, actual)
 
     def test_check_equality(self):
         with self.subTest('check being equal'):
-            i4 = PurelyImaginaryNumber(MyInt(4))
-            i2_4_1 = ImaginaryNumber(2, i4)
-            i2_4_2 = ImaginaryNumber(2, i4)
+            i2_4_1 = ImaginaryNumber(2, MyInt(4))
+            i2_4_2 = ImaginaryNumber(2, MyInt(4))
             self.assertTrue(i2_4_1 == i2_4_2)
 
         with self.subTest('check not being equal'):
-            i4 = PurelyImaginaryNumber(MyInt(4))
-            i2_4 = ImaginaryNumber(2, i4)
-            i2 = PurelyImaginaryNumber(MyInt(2))
-            i2_2 = ImaginaryNumber(2, i2)
+            i2_4 = ImaginaryNumber(2, MyInt(4))
+            i2_2 = ImaginaryNumber(2, MyInt(2))
             self.assertFalse(i2_4 == i2_2)
+
+    def test_real_is_0(self):
+        i4 = PurelyImaginaryNumber(MyInt(4))
+        i0_4 = ImaginaryNumber(0, MyInt(4))
+        self.assertTrue(i0_4 == i4)
+
+    def test_conjugate_pure_0(self):
+        with self.subTest('start with PurelyImaginaryNum'):
+            i4 = PurelyImaginaryNumber(MyInt(4))
+            minus_i0_4 = ImaginaryNumber(0, MyInt(-4))
+            self.assertTrue(minus_i0_4.is_conjugate_to(i4))
+
+        with self.subTest('start with ImaginaryNum'):
+            i4 = PurelyImaginaryNumber(MyInt(4))
+            minus_i0_4 = ImaginaryNumber(0, MyInt(-4))
+            self.assertTrue(i4.is_conjugate_to(minus_i0_4))
 
 
 if __name__ == '__main__':
